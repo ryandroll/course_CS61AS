d8f81e230cb4044d809770b88fc8b6bc2d0abd81
Yung-Chuan Wang <ryandroll@gmail.com>
2017-04-25 22:17:25 +0800

    Remove old hw folders

--- hw06/tagged_data.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(require (planet dyoo/simply-scheme))
-(provide (all-defined-out))
-
-(define (contents tagged-data)
-  (if (pair? tagged-data)
-      (cdr tagged-data)
-      (error "Not tagged data")))
