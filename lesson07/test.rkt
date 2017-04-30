a75be5798a8ae20974754790f59da7e5799e535a
Yung-Chuan Wang <ryandroll@gmail.com>
2017-04-30 17:02:35 +0800

    Add lesson07

---
+++ lesson07/test.rkt
@@ -0,0 +1,15 @@
+;;;; SICP Chapter 3.3.2
+;;;;  Representing Queues
+;;;;
+;;;; Author @uents on twitter
+;;;;
+
+#lang racket
+
+;;; for mutable pairs and lists
+
+;; (require r5rs/init)
+;; (require compatibility/defmacro)
+
+;; (require "../misc.scm")
+;; (require "obj.scm")
