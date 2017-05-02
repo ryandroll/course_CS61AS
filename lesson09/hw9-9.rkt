81a74818ecc0ac22a247a91d350598b4cc385105
Yung-Chuan Wang <ryandroll@gmail.com>
2017-05-03 02:08:17 +0800

    Answer the hard hw9-9 Exercise!!!

--- lesson09/hw9-9.rkt
+++ lesson09/hw9-9.rkt
@@ -1,9 +1,6 @@
 ;;;; SICP Compatible
 #lang racket
-(require r5rs)
-(require "../misc.scm")
-;; (require berkeley)
-(require (only-in berkeley word))
+(require berkeley)
 
 #|
 Write the procedure cxr-name.
@@ -14,26 +11,51 @@ It should return the appropriate name for that function:
 CADDDAADAR
 |#
 
-
+;; 想法一：
 ;; support treat function as blackbox, can't use rex
 ;; find with binary-tree
+;; 想法二：
+;; 利用流的概念？產生無窮無盡的表，然後根據位址回傳值
+;; 簡單版？ cd?r
+;; 想法三：
+;; 某種 環型結構，可以包容無限的 car cdr 然後再想辦法把值傳遞出來
+;; 無解＠＠還型結構跨過去無法求值，也無法計數
+;; 想法四：
+;; 利用錯誤處理發現錯誤，然後利用錯誤資訊
 
-(define x (cons 'a 'd))
+;; 輸入 cxr 字串，輸出list
+;; 該 list 執行等效 cxr 時，不會拋出錯誤且結果為 '()
 
+(define (cxr->lst* cxr-str)
+  (cond
+    ((eq? cxr-str 'c) '())
+    ((eq? (last cxr-str) 'd) (cons '() (cxr->lst* (bl cxr-str))))
+    ((eq? (last cxr-str) 'a) (cons (cxr->lst* (bl cxr-str)) '()))
+    ((eq? (last cxr-str) 'r) (cxr->lst* (bl cxr-str)))))
 
+;; 輸入兩個 cxr 字串，輸出一個等效的 cxr 字串
+;; (cxr1 (cxr2 lst)) == (cxr3 lst)
 
-(define (expand-tree tree)
-  (set-car! tree (cons (word 'a (car tree))
-                       (word 'd (car tree))))
-  (set-cdr! tree (cons (word 'a (cdr tree))
-                       (word 'd (cdr tree)))))
+(define (append-cxr cxr1 cxr2)
+  (word 'c
+        (bf (bl cxr1))
+        (bf (bl cxr2))
+        'r))
 
-#|
-(define (test-tree n)
-  (define (recu-tree n tree)
-    (cond
-      ((= n 1) (tree))
-      (else (test-tree (- n 1)
-                       (set-car! tree (cons ))))
-      )))
-|#
+;; 捕捉例外訊息，利用拋出錯誤會顯示出處理失敗的 cxr 函數
+(define (cxr-name fn)
+  (define (loop cxr)
+    (with-handlers
+      ([exn:fail? (lambda (exn)
+                    (let* ((exn-m (exn-message exn))
+                           (add-cxr (regexp-match #px"c.{1,4}r" exn-m))
+                           (new-cxr (append-cxr (car add-cxr) cxr)))
+                      (loop new-cxr)))])
+      (begin (fn (cxr->lst* cxr)) cxr)))
+  (loop 'cr))
+
+;; test
+(define fn
+  (lambda (x) (caadar (cdadr (caddr x)))))
+
+(cxr-name fn)
